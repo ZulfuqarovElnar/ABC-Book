f4609042198cd85769339532c22dcfdfc24a110d
Elnar <116576971+ZulfuqarovElnar@users.noreply.github.com>
2023-07-13 11:14:45 +0400

    Slider hisesi ve responsive ayerlar yapildi (#3)

--- app.js
+++ app.js
@@ -12,3 +12,56 @@ navToggle.addEventListener('click', () =>{
 pages.addEventListener('click', () =>{
     navPageEl.classList.toggle('show-page');
 });
+
+        // *Sliders
+const sliders = document.querySelectorAll('.slider');
+
+const circleButtons = document.querySelectorAll('.slider-circle-btn li button');
+
+let autoSlideInterval;
+
+function activateNextSlide() {
+    let currentSlideIndex = 0;
+    for (let i = 0; i < sliders.length; i++) {
+        if (sliders[i].classList.contains('active')) {
+        currentSlideIndex = i;
+        break;
+        }
+    }
+
+    const nextSlideIndex = (currentSlideIndex + 1) % sliders.length;
+
+    sliders[currentSlideIndex].classList.remove('active');
+    sliders[nextSlideIndex].classList.add('active');
+
+    circleButtons[currentSlideIndex].classList.remove('active');
+    circleButtons[nextSlideIndex].classList.add('active');
+}
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(activateNextSlide, 9000);
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
+circleButtons.forEach((button, index) => {
+button.addEventListener('click', () => {
+    stopAutoSlide();
+
+    sliders.forEach((slider) => {
+        slider.classList.remove('active');
+    });
+    circleButtons.forEach((btn) => {
+        btn.classList.remove('active');
+    });
+
+    sliders[index].classList.add('active');
+    button.classList.add('active');
+
+    startAutoSlide();
+    });
+});
+
+startAutoSlide();
